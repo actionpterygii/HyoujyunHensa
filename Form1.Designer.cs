88654e07c352461b5b308b41329f26a73bf6586b
actionpterygii <actionpterygii@gmail.com>
2019-01-10 21:50:09 +0900

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace HH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(86, 22);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(249, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(24, 84);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(311, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(311, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 451);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "HH";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace HH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //saikoro wo futte 1 no deru kakuritu ga 1/6 dearutoki no n kai saikorowo
+        //futte 1 no deru wariai wo kanngaeru
+        //nansyuruikano n no atai ni tuite 1 no deru wariai x to
+        //sono wariai ga x tonaru kakuritu p toko kannkei wo
+        //keisann shi hyou ni matome x-p no gurafu wo kaku
+
+
+        //textbox kara
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                double d;
+                if (double.TryParse(textBox1.Text, out d))
+                {
+                    d = Convert.ToDouble(textBox1.Text);
+                    SG(d);
+                    if (d <= 100 && d >= 0)
+                    {
+                        trackBar1.Value = Convert.ToInt32(d);
+                    }
+                }
+            }
+        }
+
+
+        //atai hyouji to gurafu byouga
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar1.Value.ToString();
+            double n = trackBar1.Value;
+
+            double iti = 0.1666666667;
+            double igi = 0.8333333334;
+
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            Series HHg = new Series();
+            HHg.ChartType = SeriesChartType.Spline;
+            ChartArea area1 = new ChartArea();
+            HHg.Color = Color.FromArgb(255, 0, 0, 220);
+
+            area1.AxisX.Maximum = 1;
+            area1.AxisX.Minimum = 0;
+            area1.AxisX.Interval = 0.2;
+            area1.AxisX.MajorGrid.LineColor = Color.LightGray;
+            area1.AxisX.MajorGrid.Interval = 0.05;
+
+            area1.AxisY.Maximum = 1;
+            area1.AxisY.Minimum = 0;
+            area1.AxisY.Interval = 0.2;
+            area1.AxisY.MajorGrid.LineColor = Color.LightGray;
+            area1.AxisY.MajorGrid.Interval = 0.05;
+
+            for (double m = 0; m <= n; m++)
+            {
+                double X = Convert.ToDouble(SG((m / n) * 1000.0) / 1000.0);
+                double nCm = (KJ(n, 1)) / (KJ(m, 1) * KJ(n - m, 1));
+                double drk = (Math.Pow(iti, m));
+                double dnk = (Math.Pow(igi, n - m));
+                double Y = nCm * drk * dnk;
+                HHg.Points.Add(new DataPoint(X, Y));
+            }
+            chart1.ChartAreas.Add(area1);
+            chart1.Series.Add(HHg);
+        }
+
+
+        //kaijo suru yatu
+        double KJ(double a, double i)
+        {
+            if (a == 0)
+            {
+                return i;
+            }
+            else
+            {
+                return KJ(a - 1, a * i);
+            }
+        }
+
+
+        //shisyagonyu suru yatu
+        int SG(double b)
+        {
+            return (int)(b < 0.0 ? b - 0.5 : b + 0.5);
+        }
+
+
+        //hozon suru yatu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Images|*.png;*.bmp;*.jpg";
+            ImageFormat format = ImageFormat.Bmp;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(sfd.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                }
+                chart1.SaveImage(sfd.FileName, format);
+            }
+        }
+    }
+}
